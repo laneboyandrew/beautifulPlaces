aa02b9f43fcf3779db9ccc164c242f06e401e6a3
Сергей Фомин <sfomin@synergy.ru>
2020-10-21 19:49:18 +0300

    Got JWT token. Next: set toAsyncStorage and send to server every api call

--- components/VkAuthorize.js
+++ components/VkAuthorize.js
@@ -3,26 +3,33 @@ import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { ScrollView, useWindowDimensions } from "react-native";
 import HTML from "react-native-render-html";
 import WebView from "react-native-webview";
+import Profile from "./Profile";
+import {NavigationContainer} from "@react-navigation/native";
 
 
-export default VkAuthorize = () => {
+export default VkAuthorize = ({navigation, props}) => {
     const contentWidth = useWindowDimensions().width;
 
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [token, setToken] = useState([]);
 
     useEffect(() => {
         fetch('http://vseklushki.ru/api/v2/users/auth_vk')
-            .then((response) => response.text())
-            .then((text) => setData(text))
-            .catch((error) => console.error(error))
+            .then((response) => response.json())
+            .then((json) => setData(json))
             .finally(() => setLoading(false));
     }, []);
+
+    console.log(data)
+    // if (data){
+    //      navigation.navigate('Profile')
+    // }
 
     return (
         <View style={{ flex: 1 }}>
             {isLoading ? <ActivityIndicator/> : (
-                <WebView source={{html: data}}/>
+                <WebView source={{uri: 'http://vseklushki.ru/api/v2/users/auth_vk'}}/>
             )}
         </View>
     );
